d1d2b77ed64e59aa4011520cfdb732859885aa86
rony derra <derrarony@gmail.com>
2022-11-15 14:54:50 +0200

    vechain

--- src/index.js
+++ src/index.js
@@ -15,6 +15,5 @@ const { PORT, CRON } = env;
 
 export default server.listen(PORT, async () => {
   console.log(`Listening on port ${PORT}`);
-  const DB = await connect();
-  cron.schedule(CRON.toString(), () => exoworlds(DB));
+  exoworlds();
 });
--- src/scraper.js
+++ src/scraper.js
@@ -26,45 +26,26 @@ import { Framework } from "@vechain/connex-framework";
 import { Driver, SimpleNet } from "@vechain/connex-driver";
 import { erc721Abi } from "../erc721Abi.js";
 
-export async function exoworlds(DB) {
+export async function exoworlds() {
   try {
-    const trxs = await DB.getTrx({
-      "metadata.wrapped.contract": "0x3473c5282057D7BeDA96C1ce0FE708e890764009",
-      "metadata.wrapped.origin": "25",
-    });
-
-    if (trxs.length === 0) {
-      console.log("did not find anything");
-      return;
-    }
-
-    console.log("trxs.length:", trxs.length);
     const driver = await Driver.connect(new SimpleNet("https://mainnet.veblocks.net"));
     const connex = new Framework(driver);
 
     const abi = erc721Abi;
     const abiTokenURI = abi.find(({ name }) => name === "tokenURI");
-
-    for (let item of trxs) {
       try {
         const {
           decoded: { 0: tokenURI },
         } = await connex.thor
-          .account("0x3473c5282057D7BeDA96C1ce0FE708e890764009")
+          .account("0x32CB972cA073d253651172aa8b86C086AA21683f")
           .method(abiTokenURI)
-          .call(Number(item.metadata.wrapped.tokenId));
+          .call(10);
 
         console.log({ tokenURI });
-        const resp = await DB.updateTrx(
-          { _id: item._id },
-          { "metadata.wrapped.original_uri": tokenURI }
-        );
-        console.log(resp.value.metadata.wrapped.tokenId);
+ 
       } catch (error) {
         console.log(error.message);
-        continue;
       }
-    }
 
   } catch (error) {
     console.log(error);
