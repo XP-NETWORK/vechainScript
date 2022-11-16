c465d0587cebe82f39eafe6c4e11300e63696f6b
rony derra <derrarony@gmail.com>
2022-11-16 14:59:49 +0200

    deleted

--- src/scraper.js
+++ src/scraper.js
@@ -1,7 +1,3 @@
-import { Framework } from "@vechain/connex-framework";
-import { Driver, SimpleNet } from "@vechain/connex-driver";
-import { erc721Abi } from "../erc721Abi.js";
-
 //contract 0x3473c5282057D7BeDA96C1ce0FE708e890764009
 //origin 25
 export const exoworldsIpfs = async (DB) => {
@@ -31,48 +27,3 @@ export const exoworldsIpfs = async (DB) => {
     console.log(resp.value.metadata.wrapped);
   }
 };
-
-
-export async function exoworldsContract(DB) {
-  try {
-    const trxs = await DB.getTrx({
-      "metadata.wrapped.contract": "0x3473c5282057D7BeDA96C1ce0FE708e890764009",
-      "metadata.wrapped.origin": "25",
-    });
-
-    if (trxs.length === 0) {
-      console.log("did not find anything");
-      return;
-    }
-
-    console.log("trxs.length:", trxs.length);
-    const driver = await Driver.connect(new SimpleNet("https://mainnet.veblocks.net"));
-    const connex = new Framework(driver);
-
-    const abi = erc721Abi;
-    const abiTokenURI = abi.find(({ name }) => name === "tokenURI");
-
-    for (let item of trxs) {
-      try {
-        const {
-          decoded: { 0: tokenURI },
-        } = await connex.thor
-          .account("0x3473c5282057D7BeDA96C1ce0FE708e890764009")
-          .method(abiTokenURI)
-          .call(Number(item.metadata.wrapped.tokenId));
-
-        console.log({ tokenURI });
-        const resp = await DB.updateTrx(
-          { _id: item._id },
-          { "metadata.wrapped.original_uri": tokenURI }
-        );
-        console.log(resp.value.metadata.wrapped.tokenId);
-      } catch (error) {
-        console.log(error.message);
-        continue;
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
